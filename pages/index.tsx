a3f31f8062e77c50bbdf08534d2f51645e51e111
Adolfo Blanco <ajb156@outlook.com>
2022-07-03 13:10:19 +0200

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { MainLayout } from "../components/layouts/MainLayout";
 
-export default function HomePage({ children }) {
+export default function HomePage() {
   return (
     <MainLayout>
       <h1>Home Page</h1>
